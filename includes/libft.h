6e773e16a7dd505f8ed28a9ae624c7a5065eb7b7
Filip Farkas <ffarkas@c2r1s5.42prague.com>
2023-01-12 21:52:14 +0100

    Header file added

---
+++ includes/libft.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 19:52:46 by ffarkas           #+#    #+#             */
+/*   Updated: 2023/01/12 21:49:25 by ffarkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+#endif
