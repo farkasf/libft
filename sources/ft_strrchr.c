6d1f2d3373cceeb5128029e4985f7a597aa7ef49
Filip Farkas <ffarkas@c2r1s5.42prague.com>
2023-01-12 21:40:01 +0100

    2 functions searching for the 1st/last occurence of a character added

---
+++ sources/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffarkas <ffarkas@student.42prague.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 21:03:25 by ffarkas           #+#    #+#             */
+/*   Updated: 2023/01/12 21:31:25 by ffarkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	i;
+
+	i = ft_strlen(str);
+	while (i >= 0)
+	{
+		if (str[i] == c)
+			return ((char *) &str[i]);
+		i--;
+	}
+	return (NULL);
+}
