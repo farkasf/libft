4e26673bdadadaab9eed865950e8bde518e504e6
Filip Farkas <ffarkas@c3r7s3.42prague.com>
2023-01-14 04:20:24 +0100

    Header file updated to match the corrected functions

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 19:52:46 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/01/14 03:53:49 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/01/14 04:17:18 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,27 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(const char *str);
+int		ft_strlen(const char *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_atoi(const char *str);
+int		ft_atoi(const char *ptr);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t length);
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
-char	*ft_strchr(const char *str, int c);
-char	*ft_strrchr(const char *str, int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	*ft_memset(void *str, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *dst, int c, size_t length);
+void	*ft_memcpy(void *dst, const void *src, size_t length);
+void	*ft_memmove(void *dst, const void *src, size_t length);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *buf, int ch, size_t length);
 
