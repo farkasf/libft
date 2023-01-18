c7ca0d10891d002e7e02ebf7573ac05c509066d0
Filip Farkas <ffarkas@c1r1s7.42prague.com>
2023-01-18 12:39:43 +0100

    Strtrim: Norm error fixed

--- sources/ft_strtrim.c
+++ sources/ft_strtrim.c
@@ -6,42 +6,42 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:48:46 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/01/18 00:19:20 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/01/18 10:35:26 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int  ft_isset(char c, const char *s)
+static int	ft_isset(char c, const char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char    *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    size_t  i;
-    size_t  j;
+	size_t	i;
+	size_t	j;
 
-    if (s1 == 0)
-        return (NULL);
-    if (set == 0)
-        return (ft_strdup(s1));
-    i = 0;
-    while (ft_isset(s1[i], set) == 1)
-        i++;
+	if (s1 == 0)
+		return (NULL);
+	if (set == 0)
+		return (ft_strdup(s1));
+	i = 0;
+	while (ft_isset(s1[i], set) == 1)
+		i++;
 	if ((int)i == ft_strlen(s1))
 		return (ft_strdup(""));
-    j = ft_strlen(s1);
-    while (ft_isset(s1[j - 1], set) == 1)
-        j--;
-    return (ft_substr(s1, i, (j - i)));
+	j = ft_strlen(s1);
+	while (ft_isset(s1[j - 1], set) == 1)
+		j--;
+	return (ft_substr(s1, i, (j - i)));
 }
