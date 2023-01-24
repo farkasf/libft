0b4c2851d57a9d445e450731a434eadb65646596
farkasf <120285330+farkasf@users.noreply.github.com>
2023-01-24 17:09:02 +0100

    Substr: recoded

--- sources/ft_substr.c
+++ sources/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 22:02:56 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/01/21 02:05:48 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/01/24 17:08:41 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	size_t	j;
 	char	*str;
 
-	if (s == 0 || start < 0)
+	if (s == 0)
 		return (NULL);
 	i = 0;
-	j = 0;
-	str = malloc((len + 1) * sizeof(char));
+	if (start >= ft_strlen(s))
+		return ((char *) ft_calloc(1, sizeof(char)));
+	if (ft_strlen(s) <= start + len)
+		str = malloc((ft_strlen(s) - start + 1) * sizeof(char));
+	else
+		str = malloc((len + 1) * sizeof(char));
 	if (str == 0)
 		return (NULL);
-	while (s[i])
-	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
-		i++;
-	}
-	str[j] = '\0';
+	while (s[start] && i < len)
+		str[i++] = s[start++];
+	str[i] = '\0';
 	return (str);
 }
